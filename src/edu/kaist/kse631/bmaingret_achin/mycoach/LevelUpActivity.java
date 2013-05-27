ead1c1fc9e1aa9cc9bd55302a7c9c8867f69d195
Baptiste Maingret <baptiste.maingret@gmail.com>
2013-05-27 18:46:58 +0900

    Add activities

---
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/LevelUpActivity.java
@@ -0,0 +1,22 @@
+package edu.kaist.kse631.bmaingret_achin.mycoach;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LevelUpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_level_up);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.level_up, menu);
+		return true;
+	}
+
+}
