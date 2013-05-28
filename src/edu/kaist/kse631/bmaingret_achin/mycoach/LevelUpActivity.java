f249328fb74a0df0dbad8b06774d86305bed3389
Baptiste Maingret <baptiste.maingret@gmail.com>
2013-05-28 19:17:34 +0900

    Add BaseACtivity to LevelUp

--- src/edu/kaist/kse631/bmaingret_achin/mycoach/LevelUpActivity.java
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/LevelUpActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class LevelUpActivity extends Activity {
+public class LevelUpActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
