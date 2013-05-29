0880072c2ef69ea1f97fa0748145a27ec7c9724c
Chin Ming Wei Alvin <chin_wei_2000@hotmail.com>
2013-05-29 23:54:21 +0900

    activity project settings UI done up. a little glitch with the checkbox. will fix it some other day...

--- src/edu/kaist/kse631/bmaingret_achin/mycoach/NewProjectActivity.java
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/NewProjectActivity.java
@@ -1,9 +1,13 @@
 package edu.kaist.kse631.bmaingret_achin.mycoach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.Window;
-import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewProjectActivity extends BaseActivity {
 
@@ -19,6 +23,33 @@ public class NewProjectActivity extends BaseActivity {
 //		
 //		
 		setContentView(R.layout.activity_new_project);
+		
+		Button btnSubmit = (Button) findViewById(R.id.newProject_infoSubmitButton);
+		btnSubmit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String current_act = ((EditText) findViewById(R.id.newProject_numActLastWeek)).getText().toString();
+				String current_avg = ((EditText) findViewById(R.id.newProject_current_avgDuration)).getText().toString();
+				String goal_act = ((EditText) findViewById(R.id.newProject_goalNumAct)).getText().toString();
+				String goal_avg = ((EditText) findViewById(R.id.newProject_goalAvgDuration)).getText().toString();
+				
+				if(current_act.length() != 0 && current_avg.length() != 0 && goal_act.length() != 0
+						&& goal_avg.length() != 0){
+					Intent intent = new Intent(NewProjectActivity.this, ProjectSettingsActivity.class);
+					Bundle bundle = new Bundle();
+					bundle.putInt("current_activity", Integer.parseInt(current_act));
+					bundle.putInt("current_avg", Integer.parseInt(current_avg));
+					bundle.putInt("goal_activity", Integer.parseInt(goal_act));
+					bundle.putInt("goal_avg", Integer.parseInt(goal_avg));
+					intent.putExtras(bundle);
+					startActivity(intent);
+				}else{
+					Toast.makeText(getApplicationContext(), "All the fields are required to create a new project", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 	}
 
 	@Override
