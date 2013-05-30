2bc7f00852fc625d235589221734eac108b88c7f
Baptiste Maingret <baptiste.maingret@gmail.com>
2013-05-30 22:20:31 +0900

    Fix wrong name startActivity <-> mainActivity

--- src/edu/kaist/kse631/bmaingret_achin/mycoach/MainActivity.java
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/MainActivity.java
@@ -1,10 +1,8 @@
 package edu.kaist.kse631.bmaingret_achin.mycoach;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends BaseActivity {
 
@@ -12,18 +10,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //Event listener
-        Button btnSubmit = (Button) findViewById(R.id.newProject_submitButton);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent(MainActivity.this, NewProjectActivity.class);
-				startActivity(intent);
-			}
-		});
     }
 
 
--- src/edu/kaist/kse631/bmaingret_achin/mycoach/StartActivity.java
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/StartActivity.java
@@ -1,21 +1,36 @@
 package edu.kaist.kse631.bmaingret_achin.mycoach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends BaseActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_start);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        
+        //Event listener
+        Button btnSubmit = (Button) findViewById(R.id.newProject_submitButton);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(StartActivity.this, NewProjectActivity.class);
+				startActivity(intent);
+			}
+		});
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.start, menu);
-		return true;
-	}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 }
