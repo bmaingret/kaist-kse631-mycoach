19e7601ff26c394df66679f878fcbd193ec72764
Baptiste Maingret <baptiste.maingret@gmail.com>
2013-06-07 14:39:52 +0900

    Create link between ProjectSettings and MainActivity

--- src/edu/kaist/kse631/bmaingret_achin/mycoach/ProjectSettingsActivity.java
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/ProjectSettingsActivity.java
@@ -1,7 +1,11 @@
 package edu.kaist.kse631.bmaingret_achin.mycoach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ProjectSettingsActivity extends BaseActivity {
 
@@ -9,6 +13,16 @@ public class ProjectSettingsActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_project_settings);
+		
+		Button saveProject = (Button) findViewById(R.id.projSetting_save_project);
+		saveProject.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ProjectSettingsActivity.this, MainActivity.class);
+				startActivity(intent);
+				
+			}
+		});
 	}
 
 	@Override
