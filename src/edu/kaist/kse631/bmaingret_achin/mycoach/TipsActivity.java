a946f3ea00b53171d06c52471e5cc290e90e2e06
Baptiste Maingret <baptiste.maingret@gmail.com>
2013-06-13 02:14:18 +0900

    Add basic code for Tips

--- src/edu/kaist/kse631/bmaingret_achin/mycoach/TipsActivity.java
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/TipsActivity.java
@@ -1,7 +1,12 @@
 package edu.kaist.kse631.bmaingret_achin.mycoach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TipsActivity extends BaseActivity {
 
@@ -9,6 +14,21 @@ public class TipsActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tips);
+		
+		TipHelper helper = new TipHelper(this);
+		
+		TextView tipsTextView =  (TextView) findViewById(R.id.tips_text);
+		tipsTextView.setText(helper.getRandomTip());
+		
+		Button saveButton = (Button) findViewById(R.id.tips_save_btn);
+		saveButton.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(TipsActivity.this, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
 	}
 
 }
