c6e2784ef9a8245ee1032465b4c7d89e23ec8842
Baptiste Maingret <baptiste.maingret@gmail.com>
2013-06-12 15:42:35 +0900

    Add mockup of ProjectManager

---
+++ src/edu/kaist/kse631/bmaingret_achin/mycoach/ProjectManager.java
@@ -0,0 +1,57 @@
+package edu.kaist.kse631.bmaingret_achin.mycoach;
+
+import android.content.Context;
+
+public class ProjectManager {
+	private Context context = null;
+	
+	public ProjectManager(Context context){
+		this.context = context;
+	}
+	
+	public boolean isProjectPaused(){
+		return false;
+	}
+	
+	public void updateCalendar(){
+		
+	}
+	
+	public int getLevel(int startCount, int startAverage, int endcount, int endAverage){
+		return 0;
+	}
+	
+	public int getWeeklyAchievementPercentage(){
+		return 0;
+	}
+		
+	public boolean isLevelUp(){
+		return false;
+	}
+	
+	public void levelUp(){
+		
+	}
+	
+	public void levelDown(){
+		
+	}
+	
+	public void pauseProject(){
+		
+	}
+	
+	public void resumeProject(){
+		
+	}
+	
+	public void endProject(){
+		
+	}
+	
+	public void restartProject(){
+		
+	}
+	
+	
+}
